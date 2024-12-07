9107cd4eb25949078f8f31b838bc4c530ca22b62
Enji Cooper <ngie@FreeBSD.org>
2024-12-07 15:59:52 -0800

    Don't leak memory on abnormal termination
    This eliminates the remaining issues with leaked memory in this block
    cited by clang's LSAN functionality.
    
    Signed-off-by: Enji Cooper <ngie@FreeBSD.org>

--- atf-c++/tests.cpp
+++ atf-c++/tests.cpp
@@ -454,9 +454,7 @@ init_tcs(void (*add_tcs)(tc_vector&), tc_vector& tcs,
          const atf::tests::vars_map& vars)
 {
     add_tcs(tcs);
-    for (tc_vector::iterator iter = tcs.begin(); iter != tcs.end(); iter++) {
-        impl::tc* tc = *iter;
-
+    for (auto& tc : tcs) {
         tc->init(vars);
     }
 }
@@ -619,18 +617,22 @@ safe_main(int argc, char** argv, void (*add_tcs)(tc_vector&))
             throw usage_error("Cannot provide more than one test case name");
         INV(argc == 1);
     }
-
     tc_vector tcs;
-    if (lflag) {
-        init_tcs(add_tcs, tcs, vars);
-        errcode = list_tcs(tcs);
-    } else {
-        init_tcs(add_tcs, tcs, vars);
-        errcode = run_tc(tcs, argv[0], resfile);
+    try {
+        if (lflag) {
+            init_tcs(add_tcs, tcs, vars);
+            errcode = list_tcs(tcs);
+        } else {
+            init_tcs(add_tcs, tcs, vars);
+            errcode = run_tc(tcs, argv[0], resfile);
+        }
+    } catch (...) {
+        for (auto& tc: tcs) {
+            delete tc;
+        }
+        throw;
     }
-    for (tc_vector::iterator iter = tcs.begin(); iter != tcs.end(); iter++) {
-        impl::tc* tc = *iter;
-
+    for (auto& tc: tcs) {
         delete tc;
     }
 
